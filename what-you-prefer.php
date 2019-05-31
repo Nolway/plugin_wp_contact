36d2ce829a9d8c76a7b95a88a6cf9577007464d6
christophele <le_christophe@hotmail.fr>
2019-05-31 12:46:01 +0200

    add plugin description

---
+++ what-you-prefer.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: What you prefer
+Plugin URI: http://what-you-prefer.com
+Description: Plugin sondage
+Author: Nous
+Author URI: http://what-you-prefer.com
+Version : 0.1
+*/
+
+// require_once plugin_dir_path(__FILE__).'includes/wyp-functions.php';
