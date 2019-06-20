a4dfbb85a6767082fee18f5e48c4bbf63f36aab7
christophele <le_christophe@hotmail.fr>
2019-06-20 10:53:47 +0200

    create db tables automatically

---
+++ includes/db-functions.php
@@ -0,0 +1,42 @@
+<?php
+
+global $contact_db_version;
+$contact_db_version = '1.0';
+
+function create_plugin_database_table()
+{
+    global $wpdb, $contact_db_version;
+
+    $table_name = $wpdb->prefix.'contact';
+    $charset_collate = $wpdb->get_charset_collate();
+    $sql = "CREATE TABLE $table_name (
+        id MEDIUMINT(9) NOT NULL AUTO_INCREMENT,
+        subject TINYTEXT NOT NULL,
+        content TEXT NOT NULL,
+        email VARCHAR(150) NOT NULL,
+        PRIMARY KEY (id)
+    ) $charset_collate;";
+      
+    require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+    dbDelta($sql);
+    add_option('contact_db_version', $contact_db_version);
+    $table_name = $wpdb->prefix . 'contact';
+}
+
+function contact_install_data() {
+	global $wpdb;
+	
+	$email = 'esgiteam@myges.com';
+	$subject = 'Test';
+	$content = 'Test';
+	$table_name = $wpdb->prefix.'contact';
+	$wpdb->insert( 
+		$table_name, 
+		array( 
+			'email' => $email, 
+			'subject' => $subject, 
+			'content' => $content, 
+		) 
+	);
+}
+
--- plugin_wp_contact.php
+++ plugin_wp_contact.php
@@ -8,3 +8,7 @@ Version : 0.1
 */
 
 require_once plugin_dir_path(__FILE__). 'includes/contact-functions.php';
+require_once plugin_dir_path(__FILE__). 'includes/db-functions.php';
+
+register_activation_hook(__FILE__, 'create_plugin_database_table');
+register_activation_hook(__FILE__, 'contact_install_data');
