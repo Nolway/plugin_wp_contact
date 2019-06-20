af95e8f2a1bfc644cb66047446b86825fcd7ffc6
malek yagoub <malek.yagoub@hotmail.fr>
2019-06-20 10:18:49 +0200

    changed files names

--- includes/contact-acp-page.php
+++ includes/contact-acp-page.php
@@ -1,4 +1,4 @@
 <div class="wrap">
   <h1>Bonjour</h1>
-  <p>Ceci est mon plugin WYP polls</p>
+  <p>Ceci est mon plugin de contact</p>
 </div>
---
+++ plugin_wp_contact.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Plugin wp contact
+Plugin URI: http://wordpress.org/plugins/wyp/
+Description: Add contact form for users if they want to contact administrators
+Author: ESGI Team
+Version : 0.1
+*/
+
+require_once plugin_dir_path(__FILE__). 'includes/contact-functions.php';
