cb772fe76e2a4c8b5739aef894f82e25d94987cd
christophele <le_christophe@hotmail.fr>
2019-07-01 10:42:01 +0200

    add bootstrap style

--- includes/contact-functions.php
+++ includes/contact-functions.php
@@ -1,32 +1,35 @@
 <?php
 
+add_action('wp_footer', 'contact_btn');
+
 function contact_btn() {
     $btn = '<a href="#" class="contact-btn">
     <img class="contact-btn-img" src="'.plugins_url("plugin_wp_contact/assets/img/logo-contact.png").'" alt="Contact"></a>';
     echo $btn;
 }
-    
-add_action('wp_footer', 'contact_btn');
-
-function my_thank_you_text ( $content ) {
-    return $content .= '<p>Thank you for reading!</p>';
-}
 
 add_action('admin_menu','test_plugin_setup_menu');
 
 function test_plugin_setup_menu () {
-      add_menu_page('WP contact page', 'Contacts', 'manage_options', 'contact-plugin', 'contact_plugin_page_content' );
+    add_menu_page('WP contact page', 'Contacts', 'manage_options', 'contact-plugin', 'contact_plugin_page_content' );
 }
 
 // Register style sheet
-add_action('wp_enqueue_scripts', 'btn_plugin_styles');
+add_action('wp_enqueue_scripts', 'contact_plugin_styles');
 
-// Register style sheet
-function btn_plugin_styles() {
+function contact_plugin_styles() {
+    // CSS
     wp_register_style('plugin_wp_contact', plugins_url('plugin_wp_contact/assets/styles/plugin.css'));
+    wp_register_style('bootstrap_style', 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css');
     wp_enqueue_style('plugin_wp_contact');
+    wp_enqueue_style('bootstrap_style');
+
+    // JS
+	wp_register_script('bootstrap_js', 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js');
+    wp_enqueue_script('bootstrap_js');
 }
 
+// Créer la page contact à l'activation du plugin
 function create_contact_page() {
     $my_post = array(
         'post_title'    => wp_strip_all_tags('Contact'),
@@ -36,6 +39,5 @@ function create_contact_page() {
         'post_type'     => 'page'
     );
 
-    // Insert la page dans la bdd
     wp_insert_post($my_post);
 }
--- plugin_wp_contact.php
+++ plugin_wp_contact.php
@@ -10,6 +10,7 @@ Version : 0.1
 require_once plugin_dir_path(__FILE__). 'includes/contact-functions.php';
 require_once plugin_dir_path(__FILE__). 'includes/db-functions.php';
 
+// Hooks
 register_activation_hook(__FILE__, 'create_plugin_database_table');
 register_activation_hook(__FILE__, 'contact_install_data');
 register_activation_hook(__FILE__, 'create_contact_page');
