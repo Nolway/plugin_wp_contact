b2f57cce36407a8776987f8bf04dc11d5db5e8c1
christophele <le_christophe@hotmail.fr>
2019-07-01 12:53:42 +0200

    update form

--- views/contact-form.view.php
+++ views/contact-form.view.php
@@ -1,22 +1,23 @@
 <form>
     <div class="form-group">
-        <label for="exampleInputEmail1">Email address</label>
-        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+        <label for="firstname">Nom</label>
+        <input type="text" class="form-control" id="firstname" aria-describedby="firstname-help" placeholder="Entrez votre nom">
     </div>
     <div class="form-group">
-        <label for="exampleInputEmail1">Email address</label>
-        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+        <label for="lastname">Prénom</label>
+        <input type="text" class="form-control" id="lastname" aria-describedby="lastname-help" placeholder="Entrez votre prénom">
     </div>
     <div class="form-group">
-        <label for="exampleInputEmail1">Email address</label>
-        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+        <label for="email">Adresse e-mail</label>
+        <input type="email" class="form-control" id="email" aria-describedby="email-help" placeholder="Entrez votre adresse email">
     </div>
     <div class="form-group">
-        <label for="exampleInputPassword1">Password</label>
-        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+        <label for="subject">Sujet : </label>
+        <input type="text" class="form-control" id="subject" aria-describedby="subject-help" placeholder="Sujet du message">
     </div>
-    <button type="submit" class="btn btn-primary">Submit</button>
+    <div class="form-group">
+        <label for="content">Votre message :</label>
+        <textarea class="form-control" id="content" rows="3"></textarea>
+    </div>
+    <button type="submit" class="btn btn-primary">Envoyer</button>
 </form>
