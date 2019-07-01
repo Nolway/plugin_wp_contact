9396b5441f66a6a8ad1d125f48dcd80915e85d99
christophele <le_christophe@hotmail.fr>
2019-07-01 10:17:00 +0200

    create custom contact page into bdd on plugin activation

--- includes/contact-functions.php
+++ includes/contact-functions.php
@@ -18,10 +18,6 @@ function test_plugin_setup_menu () {
       add_menu_page('WP contact page', 'Contacts', 'manage_options', 'contact-plugin', 'contact_plugin_page_content' );
 }
 
-function contact_plugin_page_content () {
-      echo"<h1>Page du plugin de contact</h1>";
-}
-
 // Register style sheet
 add_action('wp_enqueue_scripts', 'btn_plugin_styles');
 
@@ -29,4 +25,17 @@ add_action('wp_enqueue_scripts', 'btn_plugin_styles');
 function btn_plugin_styles() {
     wp_register_style('plugin_wp_contact', plugins_url('plugin_wp_contact/assets/styles/plugin.css'));
     wp_enqueue_style('plugin_wp_contact');
+}
+
+function create_contact_page() {
+    $my_post = array(
+        'post_title'    => wp_strip_all_tags('Contact'),
+        'post_content'  => 'Contact Page',
+        'post_status'   => 'publish',
+        'post_author'   => 1,
+        'post_type'     => 'page'
+    );
+
+    // Insert la page dans la bdd
+    wp_insert_post($my_post);
 }
